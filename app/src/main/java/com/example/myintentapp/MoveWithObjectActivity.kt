7dc6044755df3c72df996a702893acc8a6b96879
wawan eka setiyawan <wawaneka567@gmail.com>
2020-02-15 20:36:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myintentapp/MoveWithObjectActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveWithObjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_object)
+    }
+}
